76e8349c6e791f2a6b4c51e2bc181113cde86ee7
Zhaohan Guan <c0097744@newcastle.ac.uk>
2022-05-03 17:17:58 +0100

    Engine build

--- CSC8599/CSC8599/src/main.cpp
+++ CSC8599/CSC8599/src/main.cpp
@@ -15,6 +15,13 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main() 
 {
+	// GLM test
+	//glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	//glm::mat4 trans = glm::mat4(1.0f);
+	//trans	= glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	//vec		= trans * vec;
+	//std::cout << vec.x << vec.y << vec.z << std::endl;
+
 	glfwInit();
 
 	// OpenGL Ver3.3
--- CSC8599/Linking/include/glm/out/build/x64-Debug/cmake_install.cmake
+++ CSC8599/Linking/include/glm/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/CGE/CSC8599/Project/CSC8599-Graphics/CSC8599/Linking/include/glm
+# Install script for directory: C:/Users/c0097744/GitHub/CSC8599-Graphics/CSC8599/Linking/include/glm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/CGE/CSC8599/Project/CSC8599-Graphics/CSC8599/Linking/include/glm/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/c0097744/GitHub/CSC8599-Graphics/CSC8599/Linking/include/glm/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/CGE/CSC8599/Project/CSC8599-Graphics/CSC8599/Linking/include/glm/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/c0097744/GitHub/CSC8599-Graphics/CSC8599/Linking/include/glm/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
