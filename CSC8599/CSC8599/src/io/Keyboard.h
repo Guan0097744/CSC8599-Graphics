6416d6b8dfa915503384d112a71e9c7129a7e4dd
Zhaohan Guan <c0097744@newcastle.ac.uk>
2022-05-12 14:43:26 +0100

    Engine Build

--- CSC8599/CSC8599/src/io/Keyboard.h
+++ CSC8599/CSC8599/src/io/Keyboard.h
@@ -6,37 +6,26 @@
 
 class Keyboard {
 public:
-	/*
-		static callback
-	*/
-
-	// key state changed
+	// Key state changed
 	static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
-	/*
-		static accessors
-	*/
-
-	// get key state
+	// Get key state
 	static bool Key(int key);
 
-	// get if key recently changed
+	// Get if key recently changed
 	static bool KeyChanged(int key);
 
-	// get if key recently changed and is up
+	// Get if key recently changed and is up
 	static bool KeyWentUp(int key);
 
-	// get if key recently changed and is down
+	// Get if key recently changed and is down
 	static bool KeyWentDown(int key);
 
 private:
-	/*
-		static keyboard values
-	*/
-
-	// key state array (true for down, false for up)
+	// Key state array (true for down, false for up)
 	static bool Keys[];
-	// key changed array (true if changed)
+
+	// Key changed array (true if changed)
 	static bool KeysChanged[];
 };
 
